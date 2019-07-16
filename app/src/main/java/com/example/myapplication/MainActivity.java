7f81f1a43f00e55b1e5066776842bd4d3a9feec0
V. Alfonso Pérez Espino <taxgype1@gmail.com>
2019-07-16 17:28:30 -0500

    Faltante

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//co
         mToolbar = findViewById(R.id.toolbar_2);
 
         setSupportActionBar(mToolbar);
